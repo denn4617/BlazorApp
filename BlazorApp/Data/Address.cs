5e84bf074218f03963db9d40f27d31c1c6903350
dunnis <dunnis2k@pop-os.localdomain>
2022-04-26 15:52:32 +0200

    still researching

---
+++ BlazorApp/Data/Address.cs
@@ -0,0 +1,13 @@
+namespace BlazorApp.Data;
+
+public class Address
+{
+    public string Id { get; set; }
+    public string StreetCode { get; set; }
+    public string StreetName { get; set; }
+    public string HouseNumber { get; set; }
+    public string Floor { get; set; }
+    public string Door { get; set; }
+    public string PostalCode { get; set; }
+    
+}
